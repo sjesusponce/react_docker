34217eba75523d9feffe362c7b4e1bfccc1e61a2
apescape <ingsjesuspisturiz@gmail.com>
2019-08-14 17:53:29 -0500

    Corrección #22

--- startDeploy.sh
+++ startDeploy.sh
@@ -6,7 +6,11 @@ EXEC1=`docker push registry.heroku.com/$1/web`
 
 echo $EXEC1
 
-EXEC2=`curl -n -X PATCH "${URL}" -d "{'updates':[{'type': 'web', 'docker_image': "${2}"}]}" -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer "${3}""`
+DATA= "{'updates':[{'type': 'web', 'docker_image': "${2}"}]}"
+
+echo $DATA
+
+EXEC2=`curl -n -X PATCH "${URL}" -d "${DATA}" -H "Content-Type: application/json" -H "Accept: application/vnd.heroku+json; version=3.docker-releases" -H "Authorization: Bearer "${3}""`
 
 echo $EXEC2
 
