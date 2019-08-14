b71aa4e068ec60bb0d715236aedc5c4e3f4368dc
apescape <ingsjesuspisturiz@gmail.com>
2019-08-14 17:25:03 -0500

    Corrección #17

--- startDeploy.sh
+++ startDeploy.sh
@@ -2,7 +2,9 @@ URI="https://api.heroku.com/apps/"
 FORM="/formation"
 URL=$URI$0$FORM
 
-EXEC=`curl -n -X PATCH "${URL}" -d "{'updates':[{'type': 'web', 'docker_image': '"${1}"'}]}" -H 'Content-Type: application/json' -H 'Accept: application/vnd.heroku+json; version=3.docker-releases'`
+echo `docker push registry.heroku.com/$0/web`
+
+EXEC2=`curl -n -X PATCH "${URL}" -d "{'updates':[{'type': 'web', 'docker_image': '"${1}"'}]}" -H 'Content-Type: application/json' -H 'Accept: application/vnd.heroku+json; version=3.docker-releases'`
 
 echo $EXEC
 
